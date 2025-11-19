e528f6a8506eb52fb53488b3eeb6dbbd18c36acc
sivasagar <sivasagar605@gmail.com>
2025-11-19 11:14:45 -0500

    i changed scanf mistakes for doble codes and ampercent

--- c lang/sum.c
+++ c lang/sum.c
@@ -18,9 +18,9 @@ int main()
 	int num1, num2, sum;
 	printf("To add two numbers.\n");
 	printf("Enter the first number: ");
-	scanf(%i, num1);
+	scanf("%i", &num1);
 	printf("Enter the second number: ");
-	scanf(%i, num2);
+	scanf("%i", &num2);
 	sum = num1 + num2;
 	printf("The sum of %i and %i is %i.\n", num1, num2, sum);
 
