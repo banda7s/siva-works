8b6a982a254d55969de2e51efc167d5d96ff2bac
sivasagar <sivasagar605@gmail.com>
2025-11-21 10:26:34 -0500

    natural number upto n

---
+++ c lang/natural_number_upto_n.c
@@ -0,0 +1,35 @@
+/*
+step-1: start
+step-2: print "Upto which number you want to print natural numbers? "
+step-3: read Count
+step-4: print "The natural numbers upto " + Count + " are: "
+step-5: Counter = 1
+step-6: if ( Counter < Count ):
+          print Counter + ", "
+          Counter = Counter + 1
+          goto step-6
+step-7: print Counter + ".\n"
+step-8: stop
+*/
+
+
+#include <stdio.h>
+
+int main ()
+{
+
+    int count, counter;
+    printf("Upto which number you want to print natural numbers? ");
+    scanf("%i", &Count);
+    printf("The natural numbers upto %i are: ",count);
+    Counter = 1;
+    while( Counter < Count )
+    {
+        printf("%i, ", counter);
+        Counter = Counter + 1;
+    }
+    printf("%i.\n",counter);
+
+    return 0;
+}
+
