ca72f4373081a05d022893c943f1135f2827316e
sivasagar <sivasagar605@gmail.com>
2025-12-21 22:33:44 -0500

    comparison of two numbers v1 added

---
+++ python/comparison_of_two_numbers_v1.py
@@ -0,0 +1,7 @@
+print("Comparison between two numbers.")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+if num1 > num2:
+    print(num1," is greater than ", num2,".", sep="")
+else:
+    print(num2," is greater than ", num1,".", sep="")
